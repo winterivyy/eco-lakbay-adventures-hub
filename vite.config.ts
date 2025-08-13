ecfced97b563257714f265bdd1a2ac46fc755ac2
winterivyy <johnleomedina@gmail.com>
2025-08-13 22:06:33 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 3000,
-  },
+  
   plugins: [
     react(),
     mode === 'development' &&
