2394da4a0a76219ed88a0ba27b400965744d9203
winterivyy <johnleomedina@gmail.com>
2025-08-13 23:59:41 +0800

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
-import heroImage from "@/assets/hero-ecolakbay.jpg";
+import heroImage from "@/assets/bg.jpg";
 import { useNavigate } from "react-router-dom";
 import { useToast } from "@/hooks/use-toast";
 import { useState } from "react";
