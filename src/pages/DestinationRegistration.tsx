bc6e0c3d3e7bf6fcac0aa50e8aa6874b53d962ba
winterivyy <johnleomedina@gmail.com>
2025-08-17 17:36:29 +0800

    Update DestinationRegistration.tsx

--- src/pages/DestinationRegistration.tsx
+++ src/pages/DestinationRegistration.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import Navigation from "@/components/Navigation";
 import Footer from "@/components/Footer";
