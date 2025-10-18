1a7c988479a55fae527e40836e5fe48e72371b02
SeanMgcls <magcalassean@gmail.com>
2025-10-18 16:13:49 +0800

    Update VideosSection.tsx

--- src/components/VideosSection.tsx
+++ src/components/VideosSection.tsx
@@ -11,7 +11,7 @@ const VIDEOS: Video[] = [
   {
     id: "v1",
     title: "Sustainable Travel Tips",
-    youtubeId: "ddhzL5_TY4lglnTk", // replace with real ids
+    youtubeId: "k392842555s", // replace with real ids
     description: "Top ways to travel sustainably and reduce your footprint.",
   },
   {
